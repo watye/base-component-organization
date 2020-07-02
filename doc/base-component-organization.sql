e85774a2434db15e58c74c70af7c06bc1cf8ba72
watye <watye@qq.com>
2020-07-02 11:45:26 +0800

    feat：压力测试修改，租户表达到50万数据量，注册和登录接口变慢，耗时达到2s以上

--- doc/base-component-organization.sql
+++ doc/base-component-organization.sql
@@ -1,42 +1,42 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : 本地mysql
+ Source Server         : 23环境
  Source Server Type    : MySQL
- Source Server Version : 50727
- Source Host           : localhost:3306
- Source Schema         : base-component-organization
+ Source Server Version : 50720
+ Source Host           : 172.31.118.23:3306
+ Source Schema         : org_test
 
  Target Server Type    : MySQL
- Target Server Version : 50727
+ Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 24/06/2020 17:55:37
+ Date: 02/07/2020 11:41:54
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for t_member_info
+-- Table structure for t_tenant_info
 -- ----------------------------
-DROP TABLE IF EXISTS `t_member_info`;
-CREATE TABLE `t_member_info`  (
+DROP TABLE IF EXISTS `t_tenant_info`;
+CREATE TABLE `t_tenant_info`  (
   `tenant_id` bigint(20) UNSIGNED NOT NULL COMMENT '租户id',
-  `member_id` bigint(20) UNSIGNED NOT NULL COMMENT '成员id',
-  `member_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '成员名称',
-  `password` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码md5值',
-  `password_salt` char(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '盐值时间戳',
-  `sex` tinyint(2) UNSIGNED NOT NULL COMMENT '性别：1男 2女 0未知',
-  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '电子邮件',
-  `mobile` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
-  `is_deleted` tinyint(2) UNSIGNED NOT NULL COMMENT '是否删除：1是 0否',
+  `tenant_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '租户名称',
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码md5值',
+  `password_salt` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '盐值时间戳',
+  `phone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '绑定手机号',
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '绑定邮箱',
+  `is_enable` tinyint(2) UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否启用 1是，0否',
   `last_login_time` datetime(0) NOT NULL COMMENT '最后登录时间',
   `last_login_ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '最后登录IP',
   `gmt_create` datetime(0) NOT NULL COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`member_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`tenant_id`) USING BTREE,
+  UNIQUE INDEX `idx_phone`(`phone`) USING BTREE,
+  UNIQUE INDEX `idx_email`(`email`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '租户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_organization_info
@@ -79,23 +79,24 @@ CREATE TABLE `t_organization_member`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '组织成员表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for t_tenant_info
+-- Table structure for t_member_info
 -- ----------------------------
-DROP TABLE IF EXISTS `t_tenant_info`;
-CREATE TABLE `t_tenant_info`  (
+DROP TABLE IF EXISTS `t_member_info`;
+CREATE TABLE `t_member_info`  (
   `tenant_id` bigint(20) UNSIGNED NOT NULL COMMENT '租户id',
-  `tenant_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '租户名称',
-  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码md5值',
-  `password_salt` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '盐值时间戳',
-  `phone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '绑定手机号',
-  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '绑定邮箱',
-  `is_enable` tinyint(2) UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否启用 1是，0否',
+  `member_id` bigint(20) UNSIGNED NOT NULL COMMENT '成员id',
+  `member_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '成员名称',
+  `password` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码md5值',
+  `password_salt` char(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '盐值时间戳',
+  `sex` tinyint(2) UNSIGNED NOT NULL COMMENT '性别：1男 2女 0未知',
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '电子邮件',
+  `mobile` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
+  `is_deleted` tinyint(2) UNSIGNED NOT NULL COMMENT '是否删除：1是 0否',
   `last_login_time` datetime(0) NOT NULL COMMENT '最后登录时间',
   `last_login_ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '最后登录IP',
   `gmt_create` datetime(0) NOT NULL COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`tenant_id`) USING BTREE,
-  UNIQUE INDEX `idx_phone_email`(`phone`, `email`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '租户表' ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`member_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
